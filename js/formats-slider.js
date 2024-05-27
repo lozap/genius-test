1ffa6468de1d6491b59dff032b5ec53927a012ee
Anatol Lozinskyy <lozinskyy@gmail.com>
2024-05-27 20:59:19 +0300

    Formats section adaptive finished

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -3,15 +3,15 @@ const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 20,
   breakpoints: {
-    768: {
+    420: {
       slidesPerView: 2,
     },
-    1280: {
+    768: {
       slidesPerView: 3,
     },
   },
   pagination: {
-    el: ".pagination",
+    el: ".swiper-pagination",
     bulletClass: "pagination__button",
     bulletActiveClass: "pagination__button--active",
   },
